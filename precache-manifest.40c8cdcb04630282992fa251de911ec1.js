b7baaee8ced04ef1e3179948217b7b10eff4ff58
Sahil <sahilmanchanda92@gmail.com>
2020-07-25 19:24:38 +0530

    Updates

---
+++ precache-manifest.40c8cdcb04630282992fa251de911ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8999d4958570664c386b4caf91233",
+    "url": "/React-ToDoList/index.html"
+  },
+  {
+    "revision": "b9009944586c57000fca",
+    "url": "/React-ToDoList/static/css/main.0ab135a3.chunk.css"
+  },
+  {
+    "revision": "34649964f1174cc33aa5",
+    "url": "/React-ToDoList/static/js/2.aa8b5ee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoList/static/js/2.aa8b5ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9009944586c57000fca",
+    "url": "/React-ToDoList/static/js/main.b7c9a18b.chunk.js"
+  },
+  {
+    "revision": "586540fb3605584b40b7",
+    "url": "/React-ToDoList/static/js/runtime-main.dac6832b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
